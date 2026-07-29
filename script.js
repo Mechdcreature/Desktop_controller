3b6d5e7271e7d5eccb9f6543911d56fc3e986cce
Mechdcreature <84202514+Mechdcreature@users.noreply.github.com>
2026-07-29 12:01:39 +0530

    Add files via upload
    File uploaded successfully

---
+++ script.js
@@ -0,0 +1,50 @@
+// MQTT Broker
+const broker = "wss://019e795807a14c69987c0d11c60eca33.s1.eu.hivemq.cloud:8884/mqtt";
+
+const options = {
+    username: "esp32",
+    password: "esp32@123",
+    clean: true,
+    connectTimeout: 4000,
+    clientId: "web_" + Math.random().toString(16).substr(2, 8)
+};
+
+// Connect
+const client = mqtt.connect(broker, options);
+
+// Status
+const status = document.getElementById("status");
+
+client.on("connect", function () {
+
+    console.log("Connected");
+
+    status.innerHTML = "🟢 Connected";
+
+});
+
+client.on("error", function (err) {
+
+    console.log(err);
+
+    status.innerHTML = "🔴 Connection Failed";
+
+});
+
+// Servo 1 Button
+document.getElementById("servo1").onclick = function () {
+
+    client.publish("home/servo1", "TRIGGER");
+
+    console.log("Servo1 Trigger");
+
+};
+
+// Servo 2 Button
+document.getElementById("servo2").onclick = function () {
+
+    client.publish("home/servo2", "TRIGGER");
+
+    console.log("Servo2 Trigger");
+
+};
